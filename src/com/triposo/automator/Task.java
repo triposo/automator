f3e20c3e4a9fda4061821360266d141edb3683fc
Alex Băluț <alexandru.balut@gmail.com>
2012-06-12 17:29:58 +0200

    Allow using Chrome instead of Firefox

--- src/com/triposo/automator/Task.java
+++ src/com/triposo/automator/Task.java
@@ -1,22 +1,12 @@
 package com.triposo.automator;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.FindBy;
-import org.yaml.snakeyaml.Yaml;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-
-import static com.google.common.base.Predicates.not;
-import static org.junit.Assert.assertTrue;
 
 public abstract class Task {
   protected WebDriver driver;
@@ -26,7 +16,11 @@ public abstract class Task {
     try {
       properties.load(new FileInputStream("local.properties"));
 
-      driver = new FirefoxDriver();
+      if (properties.getProperty("browser", "firefox").equals("chrome")) {
+        driver = new ChromeDriver();
+      } else {
+        driver = new FirefoxDriver();
+      }
       driver.manage().window().setSize(new Dimension(1200, 1000));
       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
