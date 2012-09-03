1baf0dd21e7dfa13fccac1b16f91db76ce5d0461
Jon Tirsen <tirsen@triposo.com>
2012-09-03 15:12:55 +0200

    Check "Waiting for Upload" for an app.

---
+++ src/com/triposo/automator/itunesconnect/CheckWaitingForUpload.java
@@ -0,0 +1,46 @@
+package com.triposo.automator.itunesconnect;
+
+import com.triposo.automator.Task;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Iterator;
+import java.util.Map;
+
+public class CheckWaitingForUpload extends Task {
+
+  public static void main(String[] args) throws Exception {
+    new CheckWaitingForUpload().run();
+  }
+
+  public void doRun() throws Exception {
+    Integer appleId = Integer.parseInt(getProperty("apple_id"));
+
+    ManageApplicationsPage manageApplicationsPage = gotoItunesConnect(getProperty("itunes.username"), getProperty("itunes.password")).gotoManageApplications();
+    SearchResultPage searchResultPage = manageApplicationsPage.searchByAppleId(appleId);
+    AppSummaryPage appSummaryPage = searchResultPage.clickFirstResult();
+    if (appSummaryPage.containsText("Waiting For Upload")) {
+      System.out.println("YES");
+    } else {
+      System.out.println("NO");
+    }
+  }
+
+  private MainPage gotoItunesConnect(String username, String password) {
+    driver.get("https://itunesconnect.apple.com");
+    if (driver.findElement(By.cssSelector("body")).getText().contains("Password")) {
+      SigninPage signinPage = new SigninPage(driver);
+      signinPage.signin(username, password);
+    }
+    try {
+      WebElement continueButton = driver.findElement(By.cssSelector("img.customActionButton"));
+      continueButton.click();
+    } catch (NoSuchElementException e) {
+    }
+    return new MainPage(driver);
+  }
+}
