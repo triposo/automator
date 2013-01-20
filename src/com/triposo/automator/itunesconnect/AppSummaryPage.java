15c7b0902e4c9bd9c611fb7449153159d3371629
Alexandru Băluț <alex@triposo.com>
2013-01-20 17:21:31 +0100

    rename

--- src/com/triposo/automator/itunesconnect/AppSummaryPage.java
+++ src/com/triposo/automator/itunesconnect/AppSummaryPage.java
@@ -7,27 +7,27 @@ import org.openqa.selenium.support.FindBy;
 
 class AppSummaryPage extends Page {
   @FindBy(xpath = "//div[contains(@class, 'version-container') and contains(h3/text(), 'Current Version')]/div[contains(@class, 'app-icon')]/a")
-  WebElement leftHandSideVersionDetails;
+  WebElement currentVersionDetails;
   // This has to work with both "Add Version" and "View Details".
   @FindBy(xpath = "//div[contains(@class, 'version-container') and contains(h3/text(), 'New Version')]/div[contains(@class, 'app-icon')]/a")
-  WebElement rightHandSideVersionDetails;
+  WebElement newVersionDetails;
 
   public AppSummaryPage(WebDriver driver) {
     super(driver);
   }
 
-  public VersionDetailsPage clickNewVersionViewDetails() {
-    rightHandSideVersionDetails.click();
-    return new VersionDetailsPage(driver);
-  }
-
   public VersionDetailsPage clickCurrentVersionViewDetails() {
-    leftHandSideVersionDetails.click();
+    currentVersionDetails.click();
     return new VersionDetailsPage(driver);
   }
 
   public NewVersionPage clickAddVersion() {
-    rightHandSideVersionDetails.click();
+    newVersionDetails.click();
     return new NewVersionPage(driver);
+  }
+
+  public VersionDetailsPage clickNewVersionViewDetails() {
+    newVersionDetails.click();
+    return new VersionDetailsPage(driver);
   }
 }
